62720c185fa986ef41ffef3c4dc9c56e4bf9379f
Admin-Felipe <153136665+Admin-Felipe@users.noreply.github.com>
2023-12-15 02:06:39 -0300

    (ALFA) - v3.2.9
    * Atualização Inútil sem efeito algum

---
+++ model/carregamento_garantido.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Se todos os recursos estiverem carregados, torne o conteúdo visível
+    document.body.classList.remove('hidden');
+ });
+ 
